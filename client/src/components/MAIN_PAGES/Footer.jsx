7228a41c20d27d8f5b23ae68b134e1855f87c038
Aman Singh <100095370+SSShogunn@users.noreply.github.com>
2023-11-22 22:11:34 +0530

    Update Footer.jsx

--- client/src/components/MAIN_PAGES/Footer.jsx
+++ client/src/components/MAIN_PAGES/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="fixed-bottom" style={{width:"500px"}}>
       <footer className="footer text-white p-3">
         <h4>SpiceSafari</h4>
-        <p>&copy; {new Date().getFullYear()} SpiceSafari. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} SpiceSafari.</p>
       </footer>
     </div>
   );
