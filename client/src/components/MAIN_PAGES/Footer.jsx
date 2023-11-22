f6005e952cfe29ae6ddb81543af25d2cddad167e
Aman Singh <100095370+SSShogunn@users.noreply.github.com>
2023-11-22 05:40:44 +0530

    Update Footer.jsx

--- client/src/components/MAIN_PAGES/Footer.jsx
+++ client/src/components/MAIN_PAGES/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="wrapper">
+    <div className="fixed-bottom" style={{width:"500px"}}>
       <footer className="footer text-white p-3">
         <h4>SpiceSafari</h4>
         <p>&copy; {new Date().getFullYear()} SpiceSafari. All rights reserved.</p>
