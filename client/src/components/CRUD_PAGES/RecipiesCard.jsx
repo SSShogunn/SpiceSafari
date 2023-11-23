9e9c3b0a13f6ddefcdef3cef63598888708ce04f
Aman Singh <100095370+SSShogunn@users.noreply.github.com>
2023-11-24 02:17:36 +0530

    Update RecipiesCard.jsx

--- client/src/components/CRUD_PAGES/RecipiesCard.jsx
+++ client/src/components/CRUD_PAGES/RecipiesCard.jsx
@@ -3,19 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function FavouriteCard({ recipes }) {
   return (
-    <>
-      <div className="col">
-        <div className="card h-100">
-          <img style={{ maxHeight: "300px" }} src={recipes.imageUrl} alt={recipes.name} className="card-img-top" />
-          <div className="card-body" >
-            <h5 className="card-title">{recipes.name}</h5>
-          </div>
-          <div className="card-footer">
-            <small className="text-body-secondary">{recipes.dateAdded}</small>
-            <Link to={`/recipes/${recipes._id}`} className="btn btn-outline-primary float-end">View Recipe</Link>
-          </div>
+    <div className="col">
+      <div className="card h-100" > <img
+          style={{ maxHeight: "300px" }}
+          src={recipes.imageUrl}
+          alt={recipes.name}
+          className="card-img-top"
+        />
+        <div className="card-blur"> </div>
+        <div className="card-body">
+          <h5 className="card-title">{recipes.name}</h5>
+        </div>
+        <div className="card-footer">
+          <small className="text-muted">{recipes.dateAdded}</small>
+          <Link
+            to={`/recipes/${recipes._id}`}
+            className="btn btn-outline-primary float-end"
+          >
+            View Recipe
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
-};
+}
